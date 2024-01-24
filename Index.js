382b8fd55ba11546853250cfdc5b724d9606be7c
Abhinav Lade <abhinavlade@gmail.com>
2024-01-24 15:32:31 +0530

    Modification

--- Index.js
+++ Index.js
@@ -1,16 +1,18 @@
-//document.getElementById("count-el").innerText=5;
 let countEl=document.getElementById("count-el");
 console.log(countEl);
 let count=0;
 function increment()
 {
     count+=1;
-    countEl.innerText=count;
-    //console.log(count);   
+    countEl.textContent=count;
 }
 
 let saveEl=document.getElementById("save-el");
-function display()
-{   
-    console.log(count);
+
+function save()
+{ 
+    let countStr=" "+count+" -";
+    saveEl.textContent+=countStr;  
+    count=0;
+    countEl.textContent=0;
 }
